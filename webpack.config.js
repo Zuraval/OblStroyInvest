a3194ea0258fa0556b18b1b2fa17ec31fba1429c
Владимир Гуран <vladimirguran@MacBook-Air-Vladimir.local>
2025-11-06 02:54:03 +0300

    fix: font and header footer

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,13 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name][ext]'
+          },
+        },
+        {
           test: /\.pug$/,
           use: ['@webdiscus/pug-loader'],
         },
