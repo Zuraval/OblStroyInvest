8463497200a820ea270e0a0d211f8bfdc260e2a0
Vladimir Guran <vladimirguran08@gmail.com>
2026-01-10 18:01:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -727,6 +727,23 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+/*-------swiper-portfolio---------*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.swiper-portfolio', {
+    modules: [Pagination, Navigation],
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+});
 
 /*-------detail-news-slider-image---------*/
 
